776ae919ffc70963d40fd69bccf96c2af9990280
Norman Richards <orb@nostacktrace.com>
2012-08-23 11:12:14 -0500

    UA integration

--- fencester/fencester/FNSAppDelegate.m
+++ fencester/fencester/FNSAppDelegate.m
@@ -7,10 +7,11 @@
 //
 
 #import "FNSAppDelegate.h"
-
 #import "FNSFirstViewController.h"
-
 #import "FNSSecondViewController.h"
+
+#import "UAirship.h"
+#import "UAPush.h"
 
 @implementation FNSAppDelegate
 
@@ -30,48 +31,73 @@
     self.tabBarController.viewControllers = @[viewController1, viewController2];
     self.window.rootViewController = self.tabBarController;
     [self.window makeKeyAndVisible];
+    
+    
+    //Init Airship launch options
+    NSDictionary* takeOffOptions =
+    @{
+        UAirshipTakeOffOptionsLaunchOptionsKey:launchOptions ? launchOptions : @{},
+        UAirshipTakeOffOptionsAirshipConfigKey: @{
+            @"DEVELOPMENT_APP_KEY": @"dL3-GcxfRWCROH9Frwj7Gg",
+            @"DEVELOPMENT_APP_SECRET": @"wahU43hySDKyfO8yKLcZgQ",
+            @"APP_STORE_OR_AD_HOC_BUILD": @"NO",
+        }
+    };
+    
+        
+    [UAirship takeOff:takeOffOptions];        
+    [[UAPush shared] registerForRemoteNotificationTypes:(UIRemoteNotificationTypeAlert)];
     return YES;
+}
+
+
+- (void)application:(UIApplication *)app didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)devToken {
+    NSLog(@"registered - got %@", devToken);
+    [[UAPush shared] registerDeviceToken:devToken];
+}
+
+- (void)application:(UIApplication *)app didFailToRegisterForRemoteNotificationsWithError:(NSError *)err {
+    NSLog(@"Error in registration. Error: %@", err);
+}
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo
+{
+    NSLog(@"GOT %@", userInfo);
+    NSDictionary* aps = [userInfo objectForKey:@"aps"];
+    
+    if (aps) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"push!"
+                                                        message:[aps objectForKey:@"alert"]
+                                                       delegate:self
+                                              cancelButtonTitle:@"ok"
+                                              otherButtonTitles:nil];
+        [alert show];
+    }
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application
 {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+
 }
 
 - (void)applicationDidEnterBackground:(UIApplication *)application
 {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
 {
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
 {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    [UAirship land];
 }
 
-/*
-// Optional UITabBarControllerDelegate method.
-- (void)tabBarController:(UITabBarController *)tabBarController didSelectViewController:(UIViewController *)viewController
-{
-}
-*/
-
-/*
-// Optional UITabBarControllerDelegate method.
-- (void)tabBarController:(UITabBarController *)tabBarController didEndCustomizingViewControllers:(NSArray *)viewControllers changed:(BOOL)changed
-{
-}
-*/
 
 @end
